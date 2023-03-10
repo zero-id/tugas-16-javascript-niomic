519764775b420b22525053ed83a59bf02d48787c
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-10 19:40:59 +0700

    tugas 16 js

---
+++ tugas16.js
@@ -0,0 +1,11 @@
+function minmax() {
+    var min = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10) 
+
+    console.log(min)
+
+    var max = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5) 
+    
+    console.log(max)
+}
+
+minmax()
